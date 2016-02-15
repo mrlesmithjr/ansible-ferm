ca79c011cf79615a3a268dc0191dd0b735a2efa1
Larry Smith Jr <mrlesmithjr@gmail.coml>
2016-02-15 10:03:10 -0500

    Added CentOS tasks
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.coml>

--- provision.sh
+++ provision.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y git python-pip python-dev
+if [ -f /etc/debian_version ]; then
+  sudo apt-get update
+  sudo apt-get install -y git python-pip python-dev
+elif [ -f /etc/redhat-release ]; then
+  yum update
+  yum install -y git python-pip python-dev
+fi
 sudo pip install ansible
 sudo ansible-galaxy install -r /vagrant/requirements.yml -f
 ansible-playbook -i "localhost," -c local /vagrant/playbook.yml
